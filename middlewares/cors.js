eafbfef87fab5244f490ae4eb8d207a7bc039c77
Daniil-chi <149510726+Daniil-chi@users.noreply.github.com>
2024-05-21 20:29:52 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,11 +2,9 @@ const allowedCors = [
   "http://localhost:3000",
   "http://localhost:3001",
   "https://pindieofficial.nomoredomainswork.ru",
-  "https://pindieback.nomoredomainswork.ru/"
 ];
 function cors(req, res, next) {
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
     res.header(
       "Access-Control-Allow-Methods",
@@ -16,7 +14,6 @@ function cors(req, res, next) {
       "Access-Control-Allow-Headers",
       "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
     );
-  }
   next();
 }
 
