44f4980962d9265274d539b6ae11b7405c160f48
Daniil-chi <149510726+Daniil-chi@users.noreply.github.com>
2024-05-21 23:04:57 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,9 +7,7 @@ const allowedCors = [
 function cors(req, res, next) {
   const { origin } = req.headers;
   console.log("req.headers", req.headers);
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+  res.header("Access-Control-Allow-Origin", *);
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
 
   res.header(
